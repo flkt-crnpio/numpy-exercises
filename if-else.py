4f73167a16efcb3d373dd9d7ba129dabee1c39bd
flkt-crnpio <flkt.crnpio@gmail.com>
2020-01-13 16:45:19 -0600

    if else

---
+++ if-else.py
@@ -0,0 +1,24 @@
+# https://www.hackerrank.com/challenges/py-if-else/problem
+# Task
+# Given an integer n, perform the following conditional actions:
+#
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If is even and greater than 20, print Not Weird
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+if n%2==0 and n not in range(6,21):
+    print("Not Weird")
+else:
+    print("Weird")
